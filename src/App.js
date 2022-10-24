0afaaf9fc6a70636b16aa63cfe79b7df4ace8d36
“longgait” <longvnpost3101@gmail.com>
2022-10-24 23:24:00 +0700

    video 19

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-
 import MyComponent from './components/MyComponents';
+import React from 'react';
 
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
-
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello World IT Long
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
+class App extends React.Component {
+  
+  render(){
+    return(
     <div>
        Hello World IT Long
        <MyComponent></MyComponent>
     </div>
-  );
+    );
+  }
 }
 
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello World IT Long
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+    
+//   );
+// }
 export default App;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class MyComponent extends React.Component{
+    
+    state = {
+        name: 'longgait',
+        age: 35,
+        address: 'binh thạnh'
+    };
+
+    handleOnClick(even){
+        console.log("click my button");
+    }
+    handleOnMouseOver(even){
+        console.log(even.pageX);
+    }
     //JSX
     render(){
         return (
             <div>
-                my first component
-                {Math.random()}
+               My name {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handleOnClick}>Click me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Mouse Over me</button>
             </div>
         );
     }
