01f0082d5f1eab45931f05e65ad62766e7005035
“longgait” <longvnpost3101@gmail.com>
2022-10-25 23:16:56 +0700

    component cha lòng con

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,45 +1,11 @@
 import React from "react";
-
+import UserInfor from "./UserInfor";
 class MyComponent extends React.Component{
-    
-    state = {
-        name: 'longgait',
-        age: 35,
-        address: 'binh thạnh'
-    };
-
-    handleOnClick = (event) => {
-        // console.log("click my button");
-        // console.log('My name is :' ,this.state.name);
-
-        this.setState({
-            name: 'thanh long',
-            age: Math.floor((Math.random() *100) + 1)
-        })
-    }
-    handleOnMouseOver(event){
-        console.log(event.pageX);
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name : event.target.value
-        }) 
-    }
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-    }
     //JSX
     render(){
         return (
             <div>
-               My name {this.state.name} and I'm  {this.state.age} old
-                {/* <button onClick = {(even) => {this.handleOnClick(even)}}>Click me</button>
-                <button onMouseOver = {this.handleOnMouseOver}>Mouse Over me</button> */}
-                <form onSubmit={(event)=>{this.handleOnSubmit(event)}}>
-                    <input type='text' onChange = { (event) => {this.handleOnChangeInput(event)}} />
-                    <button>Submit</button>
-                </form>
+                <UserInfor/>
             </div>
             
         );
