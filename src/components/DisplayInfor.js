f5c419e62c3c76dd4d7511c8c3ca2b4475e1c023
“longgait” <longvnpost3101@gmail.com>
2022-10-26 23:23:24 +0700

    video 24

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class DisplayInfor extends React.Component{
+   
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
+    render(){
+        const {listUsers} = this.props;
+        // console.table(listUsers);
+        return(
+           <div>
+                <div>
+                    <span onClick={() => {this.handleShowHide()}}>
+                        {this.state.isShowListUser === true ?'Hide list users':'Show list users'}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div key={user.id} className = {user.age > 18 ?"green":"red"}>
+                                    <div>My name's {user.name}</div>
+                                    <div>My age's {user.age}</div>
+                                    <hr/>   
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+            </div> 
+        )
+    }
+}
+
+export default DisplayInfor;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,11 +1,25 @@
 import React from "react";
+import DisplayInfor from "./DisplayInfor";
 import UserInfor from "./UserInfor";
 class MyComponent extends React.Component{
+    
+    state = {
+        listUsers : [
+            {id: 1 ,name : "Chau", age : 7},
+            {id: 2 ,name : "van", age : 31},
+            {id: 3 ,name : "long", age : 35}
+        ]
+    }
+
     //JSX
     render(){
+        //DRY
         return (
             <div>
                 <UserInfor/>
+                <br /> <br/>
+                <DisplayInfor listUsers = {this.state.listUsers}/>
+                
             </div>
             
         );
