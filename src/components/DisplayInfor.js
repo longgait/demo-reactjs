a61da4bf3d99071117b09dabeb7fa7202eb7f090
“longgait” <longvnpost3101@gmail.com>
2022-10-30 14:23:41 +0700

    video 35

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,41 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import './DisplayInfor.scss';
-class DisplayInfor extends React.Component{
-   
-    state = {
-        isShowListUser: true
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-    render(){
-        const {listUsers} = this.props;
-        // console.table(listUsers);
-        return(
-           <div className="display-infor">
-                <div>
-                    <span onClick={() => {this.handleShowHide()}}>
-                        {this.state.isShowListUser === true ?'Hide list users':'Show list users'}
-                    </span>
-                </div>
-                {this.state.isShowListUser &&
-                    <div>
-                        {listUsers.map((user) => {
-                            return (
-                                <div key={user.id} className = {user.age > 18 ?"green":"red"}>
-                                    <div>My name's {user.name}</div>
-                                    <div>My age's {user.age}</div>
-                                    <hr/>   
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-            </div> 
-        )
-    }
-}
 
+//stateless k có state
+// class DisplayInfor extends React.Component{
+  
+
+//     render(){
+//         const {listUsers} = this.props;
+//         // console.table(listUsers);
+//         return(
+//            <div className="display-infor">
+                
+//                 {true &&
+//                     <div>
+//                         {listUsers.map((user) => {
+//                             return (
+//                                 <div key={user.id} className = {user.age > 18 ?"green":"red"}>
+//                                     <div>My name's {user.name}</div>
+//                                     <div>My age's {user.age}</div>
+//                                     <hr/>   
+//                                 </div>
+//                             )
+//                         })}
+//                     </div>
+//                 }
+//             </div> 
+//         )
+//     }
+// }
+
+const DisplayInfor = (props) => {
+    const {listUsers} = props;         
+    // console.table(listUsers);
+    const [isShowHideListUsers, setShowHideListUsers] = useState(true);
+    const handleShowHideListUser = () => {
+        setShowHideListUsers (!isShowHideListUsers);
+    }
+    return(
+        <div className="display-infor">
+                        <div>
+                            <span onClick={() => {handleShowHideListUser()}}>
+                                {isShowHideListUsers === true ? 'Hide list users' : 'Show list users'}
+                            </span>
+                        </div>
+                        {isShowHideListUsers &&
+                            <div>
+                                {listUsers.map((user) => {
+                                    return (
+                                        <div key={user.id} className = {user.age > 18 ?"green":"red"}>
+                                            <div>My name's {user.name}</div>
+                                            <div>My age's {user.age}</div>
+                                            <hr/>   
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        }
+        </div> 
+    )
+            
+}
 export default DisplayInfor;
